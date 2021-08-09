b04fd252c6aaf903e3122ab254e1dbdaed344a74
Mike <veldsinkie.mv@gmail.com>
2021-08-09 04:04:09 +0200

    fix: layout not hydrating during build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 const React = require("react")
 const { default: Layout } = require("./src/components/Layout")
 
-exports.wrapRootElement = ({ element }) => {
-  return <Layout>{element}</Layout>
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
 };
