562f109d1dd6d84cfa12d1a43ce53391be58f5d3
Mike <veldsinkie.mv@gmail.com>
2021-08-09 04:28:23 +0200

    fix: build error in mixed imports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from './src/components/layout';
 
-exports.wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 };
